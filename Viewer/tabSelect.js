9eb8e41b622f5a15eff832836ccdd76dddda23a1
Grady Blackwell <hattielover0131@gmail.com>
2025-04-07 12:54:17 -0600

    Fixed formatting and started scripting
    1. CSS updates for normal sized monitors
    2. JS file created for viewer and a little bit of code written

---
+++ Viewer/tabSelect.js
@@ -0,0 +1,15 @@
+var tab1title = "What is Alpha-1 Antitrypsin?";
+var tab1text ='<b>About</b><br\>Alpha-1 Antitrypsin Deficiency essentially means that you have a, well, deficiency of Alpha-1 Antitrypsin. This can happen for a couple reasons, which I\’ll cover in the genetics section. Now, you may be wondering a few things - things that will hopefully be answered right now, in the following sections.\<br></br\><b>What is Alpha-1 Antitrypsin?</b> <br\>Ok, so there’s this thing called Neutrophil: It’s naturally in your body, and it’s used to fight off infections in your lungs by basically destroying the infected tissue - that’s a good thing. What’s not a good thing is that Neutrophil also destroys healthy lung tissue. Luckily for us, however, we have something to neutralize the Neutrophil so it doesn’t damage our tissue: Alpha-1 Antitrypsin(AAT). AAT is a compound that forms in your liver then moves through your bloodstream until it reaches your lungs, after which it neutralizes the neutrophil and prevents excess damage.'
+var tab2title = "";
+var tab2text = "";
+var tab3title = "";
+var tab3text = "";
+var tab4title = "";
+var tab4text = "";
+var tab5title = "";
+var tab5text = "";
+function Start() {
+    document.getElementById("windowtitle").innerHTML = tab1title;
+    document.getElementById("windowtext").innerHTML = tab1text;
+
+}
